0fdf69635664645e4e4e605e3a3cc637c34549de
Hari Patel <patelhariv18@gmail.com>
2025-09-02 12:42:52 +0530

    Refined pages

--- WebApp/dental-care-web/src/pages/Result.tsx
+++ WebApp/dental-care-web/src/pages/Result.tsx
@@ -1,20 +1,24 @@
 import { Box, Typography, Paper } from "@mui/material";
+import NavBar from "../components/Navbar";
 
 export default function Result() {
   return (
-    <Box p={3}>
-      <Typography variant="h6" mb={2} color="primary">
-        Model Prediction Result
-      </Typography>
+    <Box>
+      <NavBar />
+      <Box p={3}>
+        <Typography variant="h6" mb={2} color="primary">
+          Model Prediction Result
+        </Typography>
 
-      <Paper sx={{ p: 3, borderRadius: 2 }}>
-        <Typography>
-          🦷 Prediction: <b>Cavity Detected</b>
-        </Typography>
-        <Typography>
-          Confidence: <b>87%</b>
-        </Typography>
-      </Paper>
+        <Paper sx={{ p: 3, borderRadius: 2 }}>
+          <Typography>
+            🦷 Prediction: <b>Cavity Detected</b>
+          </Typography>
+          <Typography>
+            Confidence: <b>87%</b>
+          </Typography>
+        </Paper>
+      </Box>
     </Box>
   );
 }
