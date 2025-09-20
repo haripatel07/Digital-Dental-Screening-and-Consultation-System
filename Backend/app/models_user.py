618f43b12833e383127c00de1573dbc07d9a166a
PATEL HET PIYUSHKUMAR <148608665+ihetpatel@users.noreply.github.com>
2025-09-20 21:19:51 +0530

    added user model for database

---
+++ Backend/app/models_user.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    name: Optional[str] = None
+
+class UserInDB(BaseModel):
+    id: str = Field(alias="_id")
+    email: EmailStr
+    hashed_password: str
+    name: Optional[str] = None
+    created_at: Optional[str] = None
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
